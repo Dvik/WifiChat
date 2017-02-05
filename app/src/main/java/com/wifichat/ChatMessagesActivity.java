c7bba97ed2d802f7392c5c998f758612fb9a824f
Divya <dvikash1001@gmail.com>
2017-02-05 20:21:15 +0530

    Send data from server

--- app/src/main/java/com/wifichat/ChatMessagesActivity.java
+++ app/src/main/java/com/wifichat/ChatMessagesActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -37,6 +38,16 @@ public class ChatMessagesActivity extends AppCompatActivity implements LoaderMan
 
         device = getIntent().getParcelableExtra("device");
 
+        messagesAdapter = new MessagesAdapter(ChatMessagesActivity.this, cursor);
+        recyclerView.setAdapter(messagesAdapter);
+
+        chatSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 
     private void initViews() {
@@ -64,4 +75,61 @@ public class ChatMessagesActivity extends AppCompatActivity implements LoaderMan
     public void onLoaderReset(Loader<Cursor> loader) {
         messagesAdapter.swapCursor(null);
     }
+
+    /*public class DataSendTask extends AsyncTask<Void,Void,String> {
+
+        private Context context;
+        private TextView statusText;
+
+        public DataSendTask(Context context, View statusText) {
+            this.context = context;
+            this.statusText = (TextView) statusText;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+
+                ServerSocket serverSocket = new ServerSocket(8888);
+                Socket client = serverSocket.accept();
+                InputStream inputstream = client.getInputStream();
+                sendText(inputstream, chatMessage.getText().toString());
+                serverSocket.close();
+                return ;
+            } catch (IOException e) {
+                Log.e("ChatMessagesActivity", e.getMessage());
+                return null;
+            }
+        }
+
+        *//**
+         * Start activity that can handle the JPEG image
+         *//*
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                statusText.setText("Text sent - " + result);
+            }
+        }
+
+    }
+
+    public boolean sendText(InputStream inputStream, String out) {
+        byte buf[] = new byte[1024];
+        int len;
+        long startTime=System.currentTimeMillis();
+
+        try {
+            while ((len = inputStream.read(buf)) != -1) {
+                out.write(buf, 0, len);
+            }
+            out.close();
+            inputStream.close();
+
+        } catch (IOException e) {
+            Log.d("ChatMessageActivity", e.toString());
+            return false;
+        }
+        return true;
+    }*/
 }
